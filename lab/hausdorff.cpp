a0a03fd4b859a5ac6773bf61833df175cef14eaa
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2024-09-26 18:02:46 +0200

    Fix openmp directives.

--- lab/hausdorff.cpp
+++ lab/hausdorff.cpp
@@ -113,19 +113,17 @@ Rcpp::NumericVector dist_cpp_omp(Rcpp::NumericMatrix x,
   Rcpp::NumericVector out(K);
   RcppParallel::RMatrix<double> xSafe(x);
   RcppParallel::RVector<double> outSafe(out);
+
 #ifdef _OPENMP
   #pragma omp parallel for num_threads(ncores)
+#endif
+  for (unsigned int k = 0;k < K;++k)
   {
-#endif
-    for (unsigned int k = 0;k < K;++k)
-    {
-      unsigned int i = N - 2 - std::floor(std::sqrt(-8 * k + 4 * N * (N - 1) - 7) / 2.0 - 0.5);
-      unsigned int j = k + i + 1 - N * (N - 1) / 2 + (N - i) * ((N - i) - 1) / 2;
-      outSafe[k] = hausdorff_distance_cpp(xSafe.row(i), xSafe.row(j), dimension);
-    }
-#ifdef _OPENMP
+    unsigned int i = N - 2 - std::floor(std::sqrt(-8 * k + 4 * N * (N - 1) - 7) / 2.0 - 0.5);
+    unsigned int j = k + i + 1 - N * (N - 1) / 2 + (N - i) * ((N - i) - 1) / 2;
+    outSafe[k] = hausdorff_distance_cpp(xSafe.row(i), xSafe.row(j), dimension);
   }
-#endif
+
   out.attr("Size") = N;
   out.attr("Labels") = Rcpp::seq(1, N);
   out.attr("Diag") = false;
